4762cbed068f23c7447999daaea438996f4b1b8f
Isa Egizbay <isaegizbay09@gmail.com>
2022-09-15 04:01:12 +0600

    config: setup jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,32 @@
-module.exports = {
-	moduleFileExtensions: ['ts', 'js', 'tsx', 'json'],
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+	dir: './'
+});
+
+const config = {
 	setupFilesAfterEnv: [
 		'<rootDir>/jest/setup.ts',
 		'<rootDir>/jest/nullOrAny.ts'
 	],
-	transform: {
-		'^.+\\.ts$': 'ts-jest',
-		'^.+\\.js$': 'babel-jest'
-	},
+	testEnvironment: 'jest-environment-jsdom',
 	moduleDirectories: ['node_modules', __dirname],
+	coverageDirectory: '<rootDir>/jest/coverage/',
 	collectCoverage: true,
 	collectCoverageFrom: [
-		'<rootDir>/components/**/*.vue',
-		'<rootDir>/pages/**/*.vue'
+		'<rootDir>/app/**/*.ts',
+		'<rootDir>/app/**/*.tsx',
+		'!<rootDir>/app/**/*.locale.ts',
+		'!<rootDir>/app/**/constants/*',
+		'!<rootDir>/app/**/enums/*',
+		'!<rootDir>/app/**/types/*',
+		'!<rootDir>/app/storage/*',
+		'!<rootDir>/**/index.ts',
+		'<rootDir>/components/**/*.tsx',
+		'<rootDir>/pages/**/*.tsx'
 	]
 };
+
+module.exports = createJestConfig(config);
