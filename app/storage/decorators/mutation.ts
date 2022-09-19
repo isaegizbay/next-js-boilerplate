7175ef868f28529f1aea12d74a3a90b597436119
isaegizbay <isa.egizbay@kaspi.kz>
2022-09-19 18:40:32 +0600

    fix(storage): fixed @mutation decorator

--- app/storage/decorators/mutation.ts
+++ app/storage/decorators/mutation.ts
@@ -5,7 +5,7 @@ export const mutation = (target: any, propertyKey: string, descriptor: PropertyD
 
 	descriptor.value = function (this: Module<any, any>, ...args: unknown[]) {
 		console.log(this, propertyKey);
-		this._dispatch(this._actions[propertyKey](args));
+		this._dispatch(this._actions[propertyKey](...args));
 		return method.apply(this, args);
 	}
 
